cdda4b8e175c5952cac998c8a3829491483201a1
CarbonJep <jeffrey.ramos@carbondigital.com.ph>
2018-03-20 18:00:50 +0800

    WIP js for accordion

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,22 @@
 // imports
 import '../scss/main.scss'; //importing scss file
+
+class accordion{
+	constructor(clName){
+		this.className = clName;
+		this.accord_button = document.getElementsByClassName(this.className);
+	}
+
+	activate(){
+		for (var i = 0 ; i < this.accord_button.length; i++) {
+		  	this.accord_button[i].addEventListener('click' , function(){
+				this.classList.add('c-accordion--active');
+				alert("YO!");
+		 	}); 
+		 	this.accord_button[i].classList.remove('c-accordion--active');
+		}
+	}
+}
+
+const faq = new accordion('c-accordion__wrap');
+faq.activate();
